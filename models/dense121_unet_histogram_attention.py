cbe328d2720e93abd0427bc0ceec9921069430e0
MotazAh <almotaz.ahmed@gmail.com>
2023-02-24 18:16:12 +0000

    Edited files

--- models/dense121_unet_histogram_attention.py
+++ models/dense121_unet_histogram_attention.py
@@ -7,12 +7,195 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torchvision import models as torch_models
 from torchvision.models import resnet34
 from torchvision.models.resnet import BasicBlock, Bottleneck
 
 from models.networks import _DenseBlock, _Transition, RDB, GaussianHistogram, AttentionExtractModule
-from models.warpnet import WarpNet
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.padding1 = nn.ReflectionPad2d(padding)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=0, stride=stride)
+        self.bn1 = nn.InstanceNorm2d(out_channels)
+        self.prelu = nn.PReLU()
+        self.padding2 = nn.ReflectionPad2d(padding)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=0, stride=stride)
+        self.bn2 = nn.InstanceNorm2d(out_channels)
+
+    def forward(self, x):
+        residual = x
+        out = self.padding1(x)
+        out = self.conv1(out)
+        out = self.bn1(out)
+        out = self.prelu(out)
+        out = self.padding2(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += residual
+        out = self.prelu(out)
+        return out
+
+class WarpNet(nn.Module):
+    """
+    Inputs are the res34 features
+    """
+    def __init__(self, feat1=64, feat2=128, feat3=256, feat4=512):
+        super(WarpNet, self).__init__()
+        self.feature_channel = 64
+        self.in_channels = self.feature_channel * 4
+        self.inter_channels = 256
+        # 44*44
+        self.layer2_1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(feat1, 128, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(128),
+            nn.PReLU(),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(128, self.feature_channel, kernel_size=3, padding=0, stride=2),
+            nn.InstanceNorm2d(self.feature_channel),
+            nn.PReLU(),
+        )
+
+        self.layer3_1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(feat2, 128, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(128),
+            nn.PReLU(),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(128, self.feature_channel, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(self.feature_channel),
+            nn.PReLU(),
+        )
+
+        # 22*22->44*44
+        self.layer4_1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(feat3, 256, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(256),
+            nn.PReLU(),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(256, self.feature_channel, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(self.feature_channel),
+            nn.PReLU(),
+            nn.Upsample(scale_factor=2),
+        )
+
+        # 11*11->44*44
+        self.layer5_1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(feat4, 256, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(256),
+            nn.PReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(256, self.feature_channel, kernel_size=3, padding=0, stride=1),
+            nn.InstanceNorm2d(self.feature_channel),
+            nn.PReLU(),
+            nn.Upsample(scale_factor=2),
+        )
+
+        self.layer = nn.Sequential(
+            ResidualBlock(self.feature_channel * 4, self.feature_channel * 4, kernel_size=3, padding=1, stride=1),
+            ResidualBlock(self.feature_channel * 4, self.feature_channel * 4, kernel_size=3, padding=1, stride=1),
+            ResidualBlock(self.feature_channel * 4, self.feature_channel * 4, kernel_size=3, padding=1, stride=1),
+        )
+
+        self.theta = nn.Conv2d(
+            in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1, stride=1, padding=0
+        )
+        self.phi = nn.Conv2d(
+            in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1, stride=1, padding=0
+        )
+
+        self.upsampling = nn.Upsample(scale_factor=4)
+
+    def forward(
+        self,
+        B_hist,
+        A_relu2_1,
+        A_relu3_1,
+        A_relu4_1,
+        A_relu5_1,
+        B_relu2_1,
+        B_relu3_1,
+        B_relu4_1,
+        B_relu5_1,
+        temperature=0.001 * 5,
+        detach_flag=False,
+    ):
+        batch_size = B_hist.shape[0]
+
+        # scale feature size to 44*44
+        A_feature2_1 = self.layer2_1(A_relu2_1)
+        B_feature2_1 = self.layer2_1(B_relu2_1)
+        A_feature3_1 = self.layer3_1(A_relu3_1)
+        B_feature3_1 = self.layer3_1(B_relu3_1)
+        A_feature4_1 = self.layer4_1(A_relu4_1)
+        B_feature4_1 = self.layer4_1(B_relu4_1)
+        A_feature5_1 = self.layer5_1(A_relu5_1)
+        B_feature5_1 = self.layer5_1(B_relu5_1)
+
+        # concatenate features
+        if A_feature5_1.shape[2] != A_feature2_1.shape[2] or A_feature5_1.shape[3] != A_feature2_1.shape[3]:
+            A_feature2_1 = padding_customize(A_feature2_1, A_feature5_1)
+            A_feature3_1 = padding_customize(A_feature3_1, A_feature5_1)
+            A_feature4_1 = padding_customize(A_feature4_1, A_feature5_1)
+
+        if B_feature5_1.shape[2] != B_feature2_1.shape[2] or B_feature5_1.shape[3] != B_feature2_1.shape[3]:
+            B_feature2_1 = padding_customize(B_feature2_1, B_feature5_1)
+            B_feature3_1 = padding_customize(B_feature3_1, B_feature5_1)
+            B_feature4_1 = padding_customize(B_feature4_1, B_feature5_1)
+
+        A_features = self.layer(torch.cat((A_feature2_1, A_feature3_1, A_feature4_1, A_feature5_1), 1))
+        B_features = self.layer(torch.cat((B_feature2_1, B_feature3_1, B_feature4_1, B_feature5_1), 1))
+
+        # pairwise cosine similarity
+        theta = self.theta(A_features).view(batch_size, self.inter_channels, -1)  # 2*256*(feature_height*feature_width)
+        theta = theta - theta.mean(dim=-1, keepdim=True)  # center the feature
+        theta_norm = torch.norm(theta, 2, 1, keepdim=True) + sys.float_info.epsilon
+        theta = torch.div(theta, theta_norm)
+        theta_permute = theta.permute(0, 2, 1)  # 2*(feature_height*feature_width)*256
+        phi = self.phi(B_features).view(batch_size, self.inter_channels, -1)  # 2*256*(feature_height*feature_width)
+        phi = phi - phi.mean(dim=-1, keepdim=True)  # center the feature
+        phi_norm = torch.norm(phi, 2, 1, keepdim=True) + sys.float_info.epsilon
+        phi = torch.div(phi, phi_norm)
+        f = torch.matmul(theta_permute, phi)  # 2*(feature_height*feature_width)*(feature_height*feature_width)
+        if detach_flag:
+            f = f.detach()
+
+        f_similarity = f.unsqueeze_(dim=1)
+        similarity_map = torch.max(f_similarity, -1, keepdim=True)[0]
+        similarity_map = similarity_map.view(batch_size, 1, A_feature2_1.shape[2],  A_feature2_1.shape[3])
+
+        # f can be negative
+        f_WTA = f
+        f_WTA = f_WTA / temperature
+        f_div_C = F.softmax(f_WTA.squeeze_(), dim=-1)  # 2*1936*1936;
+
+        # downsample the reference histogram
+        feature_height, feature_width = B_hist.shape[2], B_hist.shape[3]
+        B_hist = B_hist.view(batch_size, 512, -1)
+        B_hist = B_hist.permute(0, 2, 1)
+        y_hist = torch.matmul(f_div_C, B_hist)
+        y_hist = y_hist.permute(0, 2, 1).contiguous()
+        y_hist_1 = y_hist.view(batch_size, 512, feature_height, feature_width)
+
+        # upsample, downspale the wrapped histogram feature for multi-level fusion
+        upsample = nn.Upsample(scale_factor=2)
+        y_hist_0 = upsample(y_hist_1)
+        y_hist_2 = F.avg_pool2d(y_hist_1, 2)
+        y_hist_3 = F.avg_pool2d(y_hist_1, 4)
+
+        # do the same thing to similarity map
+        similarity_map_0 = upsample(similarity_map)
+        similarity_map_1 = similarity_map
+        similarity_map_2 = F.avg_pool2d(similarity_map_1, 2)
+        similarity_map_3 = F.avg_pool2d(similarity_map_1, 4)
+
+        return [(y_hist_0, similarity_map_0), (y_hist_1, similarity_map_1),
+                (y_hist_2, similarity_map_2), (y_hist_3, similarity_map_3)]
 
 class HistogramLayerLocal(nn.Module):
     def __init__(self):
